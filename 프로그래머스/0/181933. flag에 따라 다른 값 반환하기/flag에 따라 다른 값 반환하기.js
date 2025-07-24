f3181a2f01d6a44cc1d1a42d49ae20eaa0f0e5e4
문정우 <106897607+MoonjeongWoo@users.noreply.github.com>
2025-07-24 11:55:22 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.js
@@ -0,0 +1,15 @@
+function solution(a, b, flag) {
+    
+    let answer;
+    
+    if(flag == true)
+        {
+            answer = a + b;        
+        }
+    else
+        {
+            answer = a - b ;
+        }
+    
+    return answer;
+}
