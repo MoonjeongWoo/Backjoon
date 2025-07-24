a6323548b074f903dc613cf281d7783692d1a8a1
문정우 <106897607+MoonjeongWoo@users.noreply.github.com>
2025-07-24 15:08:30 +0900

    [level 0] Title: 양꼬치, Time: 3.43 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,10 @@
+function solution(n, k) {
+    let yang = 12000;
+    let drink = 2000;
+    
+    let sub = Math.floor(n/10);
+    console.log(sub)
+            return (yang * n) + (drink * (k - sub) )
+        
+        
+}
