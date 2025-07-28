6612cf35ef944d1e24910853080d76db36334ac2
문정우 <106897607+MoonjeongWoo@users.noreply.github.com>
2025-07-28 17:18:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let arr = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+    
+    for (let i = 0; i < arr.length; i++) {
+    s = s.split(arr[i]).join(i);
+    };
+    return Number(s)
+    
+}
